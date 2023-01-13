5a4996e061a9467b8da6016e5ddc06a0ab9e5fd1
unknown <rapexamrs@gmail.com>
2023-01-13 05:32:59 -0800

    make model for user Notes

---
+++ Server/web/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Notes(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField("Update")
