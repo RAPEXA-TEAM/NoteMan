b3b70c840c1ef4a27609dee703155b9d53c929f8
rapexa <rapexamrs@gmail.com>
2023-02-15 17:11:21 +0330

    add Users Token modelto django models.py

--- Server/web/models.py
+++ Server/web/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.token)
 
 class Notes(models.Model):
 
@@ -9,5 +15,6 @@ class Notes(models.Model):
     title = models.CharField(max_length=255)
     text = models.CharField(max_length=255)
     date = models.DateTimeField("Update")
+    
     def __str__(self):
         return "{}  {}  [{}]".format(self.user, self.title, self.date)
