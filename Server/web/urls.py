b6798aabf98ab4a03c4ccc4d2b7ee137de8dc852
rapexa <rapexamrs@gmail.com>
2023-02-15 17:07:13 +0330

    change cuase of changeing my os without any reason

---
+++ Server/web/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from web.views import submit_note, register, edit_note, forget, login
+
+urlpatterns = [
+    path('submit/Note/', submit_note , name='submit_note'),
+    path('register/' , register, name='register'),  
+    path('edit/Note/', edit_note, name='edit_note'),
+    path('accounts/register/', register, name='register'),
+    path('accounts/forget/', forget, name='forget'),
+    path('accounts/login/', login, name='login'),
+]
